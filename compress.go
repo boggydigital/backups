34f240479d2e2747e94a2a9da0af49aab26fd169
Bogdan Brinza <Bogdan.Brinza@hey.com>
2024-12-05 18:24:10 -0800

    Reimplementing compress as a walker function that skips links (turned out to be problematic for theo WINE prefixes dosdevices dir)

--- compress.go
+++ compress.go
@@ -3,16 +3,45 @@ package backups
 import (
 	"archive/tar"
 	"compress/gzip"
-	"errors"
+	"io"
 	"os"
 	"path/filepath"
 )
 
-func Compress(src, dst string) error {
+//func Compress(src, dst string) error {
+//
+//	if src == "" || dst == "" {
+//		return errors.New("compressing requires src and dst dirs")
+//	}
+//
+//	exportedPath := filepath.Join(dst, Filename())
+//
+//	if _, err := os.Stat(exportedPath); os.IsExist(err) {
+//		return err
+//	}
+//
+//	file, err := os.Create(exportedPath)
+//	if err != nil {
+//		return err
+//	}
+//	defer file.Close()
+//
+//	gw := gzip.NewWriter(file)
+//	defer gw.Close()
+//
+//	tw := tar.NewWriter(gw)
+//	defer tw.Close()
+//
+//	fs := os.DirFS(src)
+//
+//	if err := tw.AddFS(fs); err != nil {
+//		return err
+//	}
+//
+//	return nil
+//}
 
-	if src == "" || dst == "" {
-		return errors.New("compressing requires src and dst dirs")
-	}
+func Compress(src, dst string) error {
 
 	exportedPath := filepath.Join(dst, Filename())
 
@@ -32,11 +61,51 @@ func Compress(src, dst string) error {
 	tw := tar.NewWriter(gw)
 	defer tw.Close()
 
-	fs := os.DirFS(src)
+	tarWalker := func(path string, fi os.FileInfo, err error) error {
 
-	if err := tw.AddFS(fs); err != nil {
+		if fi.Mode().IsDir() {
+			return nil
+		}
+
+		// this takes care of linked files that are problematic for tar
+		if !fi.Mode().IsRegular() {
+			return nil
+		}
+
+		relPath, err := filepath.Rel(src, path)
+		if err != nil {
+			return err
+		}
+
+		if len(relPath) == 0 {
+			return nil
+		}
+
+		rcFile, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer rcFile.Close()
+
+		if h, err := tar.FileInfoHeader(fi, relPath); err != nil {
+			return err
+		} else {
+			h.Name = relPath
+			if err = tw.WriteHeader(h); err != nil {
+				return err
+			}
+		}
+
+		if _, err := io.Copy(tw, rcFile); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	if err = filepath.Walk(src, tarWalker); err != nil {
 		return err
 	}
 
 	return nil
+
 }
