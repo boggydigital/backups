402ca3caf30b431ad882bfb12529508ef862a4a4
Bogdan Brinza <Bogdan.Brinza@hey.com>
2025-02-06 11:15:13 -0800

    Bumping go
    Removing x/exp

--- compress.go
+++ compress.go
@@ -8,39 +8,6 @@ import (
 	"path/filepath"
 )
 
-//func Compress(src, dst string) error {
-//
-//	if src == "" || dst == "" {
-//		return errors.New("compressing requires src and dst dirs")
-//	}
-//
-//	exportedPath := filepath.Join(dst, Filename())
-//
-//	if _, err := os.Stat(exportedPath); os.IsExist(err) {
-//		return err
-//	}
-//
-//	file, err := os.Create(exportedPath)
-//	if err != nil {
-//		return err
-//	}
-//	defer file.Close()
-//
-//	gw := gzip.NewWriter(file)
-//	defer gw.Close()
-//
-//	tw := tar.NewWriter(gw)
-//	defer tw.Close()
-//
-//	fs := os.DirFS(src)
-//
-//	if err := tw.AddFS(fs); err != nil {
-//		return err
-//	}
-//
-//	return nil
-//}
-
 func Compress(src, dst string) error {
 
 	exportedPath := filepath.Join(dst, Filename())
--- go.mod
+++ go.mod
@@ -1,7 +1,5 @@
 module github.com/boggydigital/backups
 
-go 1.23.4
+go 1.23.6
 
-require github.com/boggydigital/nod v0.1.21
-
-require golang.org/x/exp v0.0.0-20241204233417-43b7b7cde48d // indirect
+require github.com/boggydigital/nod v0.1.22
